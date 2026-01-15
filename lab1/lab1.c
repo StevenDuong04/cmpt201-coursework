2204b909eababab5c5862f279e143853f1214db9
StevenDuong04 <vduong.steven@gmail.com>
2026-01-14 19:47:32 -0800

    Lab1.c Submission

---
+++ lab1/lab1.c
@@ -0,0 +1,32 @@
+// Steven Duong 301552606
+// Lab1.c
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+  char *buff = NULL;
+  size_t size = 0;
+  char *saveptr, *token;
+
+  printf("Please enter some text: ");
+  ssize_t num_char = getline(&buff, &size, stdin);
+  if (num_char == -1) {
+    perror("getline failed");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok_r(buff, " ", &saveptr);
+
+  while (token != NULL) {
+    printf(" %s\n", token);
+    token = strtok_r(NULL, " ", &saveptr);
+  }
+
+  free(buff);
+  buff = NULL;
+
+  return 0;
+}
